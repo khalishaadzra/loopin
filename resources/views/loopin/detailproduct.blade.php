4781880019fbd04f37e1919f50d206cd65ddc64e
ZalviaInasya <166111888+ZalviaInasya@users.noreply.github.com>
2025-04-13 19:04:51 +0700

    Create detailproduk.blade.php

---
+++ resources/views/loopin/detailproduct.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Produk - Loopin</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <style>
+    body {
+      font-family: 'Inter', sans-serif;
+    }
+  </style>
+</head>
+<body class="bg-white text-[#521018]">
+
+   
