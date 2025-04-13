3651064730104be785ba45553fedc22242bdc190
Khalisha Adzraini Arif <166109055+khalishaadzra@users.noreply.github.com>
2025-04-13 19:07:01 +0700

    Update category.blade.php

--- resources/views/loopin/category.blade.php
+++ resources/views/loopin/category.blade.php
@@ -62,6 +62,35 @@
     </div>
   </nav>
 
+<!-- Footer -->
+  <footer class="bg-[#521018] text-white py-20 px-6 mt-16">
+    <div class="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 text-center md:text-left">
+      <div>
+        <img src="{{ asset('footerlog.svg') }}" alt="Loopin Logo" class="h-6 mx-auto md:mx-0">
+        <p class="text-sm mt-2">We help you find your fits!</p>
+        <div class="flex justify-center md:justify-start space-x-8 mt-6 text-xl">
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+        </div>
+      </div>
+      <div>
+        <h3 class="font-semibold mb-2">Support</h3>
+        <p class="text-sm">Darussalam, Kec. Syiah Kuala<br>Kota Banda Aceh, Aceh 23111</p>
+        <p class="text-sm mt-2">loopin@gmail.com<br>+62 819 3748 1281</p>
+      </div>
+      <div>
+        <h3 class="font-semibold mb-2">Kategori</h3>
+        <ul class="text-sm space-y-1">
+          <li>Atasan</li>
+          <li>Bawahan </li>
+          <li>Dress</li>
+          <li>Lainnya</li>
+        </ul>
+      </div>
+    </div>
+    <p class="text-center text-xs mt-10">© 2025 Loopin. All rights reserved.</p>
+  </footer>
 
 </body>
 </html>
