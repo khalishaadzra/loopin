3d1af07cdb80aa19caeca9f3aba9d1abff20ca43
Khalisha Adzraini Arif <166109055+khalishaadzra@users.noreply.github.com>
2025-04-13 19:03:11 +0700

    Create category.blade.php

---
+++ resources/views/loopin/category.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Loopin - Category</title>
+  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet"/>
+  <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            primary: '#A54D4D',
+            secondary: '#E6D2AE',
+            background: '#F9F3EB',
+            dark: '#521018'
+          }
+        }
+      }
+    }
+  </script>
+  <style>
+    /* Tambahan agar scroll tersembunyi di browser */
+    .hide-scrollbar::-webkit-scrollbar {
+      display: none;
+    }
+    .hide-scrollbar {
+      -ms-overflow-style: none;
+      scrollbar-width: none;
+    }
+  </style>
+</head>
+<body class="bg-white text-[#521018]">
